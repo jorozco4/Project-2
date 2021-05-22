bd9ddf08aaad268a5717c254eaf5142358b56bc2
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-22 10:52:02 -0400

    created model file structure with data

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    content: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+  }
+);
+
+module.exports = Post;
