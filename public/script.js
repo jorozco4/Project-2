8a60bfba72513ea72ba4e26eed860a97b398da3e
Jada Desormeaux <JadaE789@gmail.com>
2021-05-25 19:25:40 -0400

    login.css

--- public/script.js
+++ public/script.js
@@ -1,19 +1,50 @@
-// const shoeName = document.getElementById("name")
+const userInfo = JSON.parse(localStorage.getItem("userInfo"));
 
-// function test() {
-// fetch("https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=100&name=nike", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "0c1a9a5c52msh230a7f2d99ccc15p10ad0djsn27e4137a93bc",
-// 		"x-rapidapi-host": "v1-sneakers.p.rapidapi.com"
-// 	}
-// })
-// .then(data => {
-// 	console.log(data);
-// })
-// .catch(err => {
-// 	console.error(err);
-// })
-// }
+const formSubmitBtn = $('#form-submit')
 
-// test()
+formSubmitBtn.click((e) => {
+    e.preventDefault()
+    if ($("#brand").val() == "" || $("#name").val() == "" || $("#releaseYear").val() == "") {
+      confirm("Please fill out all fields!");
+  
+    } else {
+      const userInfo = {
+        brand: $("#brand").val(),
+        name: $("#name").val(),
+        relYear: $("#releaseYear").val()
+      };
+      localStorage.setItem('userInfo', JSON.stringify(userInfo));
+      window.location.href = "./review.html";
+    }
+  })
+
+//   `https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=10&name=${userInfo.name}&releaseYear=${userInfo.relYear}&brand=${userInfo.brand}`
+
+function renderSneakers() {
+    const img = document.createElement("img")
+
+    fetch(`https://v1-sneakers.p.rapidapi.com/v1/sneakers?limit=10&name=${userInfo.name}&releaseYear=${userInfo.relYear}&brand=${userInfo.brand}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "0c1a9a5c52msh230a7f2d99ccc15p10ad0djsn27e4137a93bc",
+		"x-rapidapi-host": "v1-sneakers.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then (data => {
+    console.log(data.results[0])
+    console.log(data.results[0].media.smallImageUrl)
+    img.src = data.results[0].media.smallImageUrl
+    console.log(img.src)
+    $("#shoe-img").append(img)
+    $("#shoe-name").html(data.results[0].shoe)
+	  $("#relDate").html("Release Date: " + data.results[0].releaseDate)
+    $("#retailPrice").html("Current Costs: $" + data.results[0].retailPrice)
+    $("#styleID").html("Style ID: " + data.results[0].styleId)
+})
+.catch(err => {
+	console.error(err);
+});
+}
+
+renderSneakers()
