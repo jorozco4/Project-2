f3764827779f3311304ca3f8ff5013ad3d56bc52
Jesse Orozco <orozcojesse0@gmail.com>
2021-05-29 14:49:47 -0400

    added another router post & router.get to product routes.js,render a few descriptions into revew.handlebars

--- models/index.js
+++ models/index.js
@@ -3,8 +3,8 @@ const Product = require("./product");
 const User = require("./user");
 
 Review.belongsTo(Product, {
-  foreignKey:"product_id"
-})
+  foreignKey: "product_id",
+});
 
 Product.hasMany(Review, {
   foreignKey: "product_id",
@@ -15,8 +15,8 @@ Review.belongsTo(User, {
 });
 
 User.hasMany(Review, {
-  foreignKey: "user_id"
-})
+  foreignKey: "user_id",
+});
 
 module.exports = {
   User,
